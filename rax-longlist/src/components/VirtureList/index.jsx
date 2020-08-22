be20ba95c8c94a4c6e2d8964663970519984bfe0
XingGuoZM <18969176324@163.com>
2020-08-23 01:12:22 +0800

    virturelist

--- rax-longlist/src/components/VirtureList/index.jsx
+++ rax-longlist/src/components/VirtureList/index.jsx
@@ -14,10 +14,13 @@ const firstRef = createRef();
 const secondRef = createRef();
 const thirdRef = createRef();
 
+let page = 1;
 let prevDis = 2;
 let currDis = 0;
 export default () => {
-  let [page, setPage] = useState(1);
+  let [page1, setPage1] = useState(1);
+  let [page2, setPage2] = useState(2);
+  let [page3, setPage3] = useState(3);
   function handleScroll() {
     scrollRef.current._nativeNode.addEventListener('scroll', (e) => {
       let y = bottomRef.current.getBoundingClientRect().bottom;
@@ -27,38 +30,56 @@ export default () => {
       // 计算比率，检测是否到底了
       let down = 0.91;
       let up = 1.09;
+      console.log('dis', currDis < prevDis);
       // 向上滑动
       if (currDis < down && currDis < prevDis) {
-        console.log('向上滑动');
+        console.log('向上滑动', page);
 
         bottomRef.current.style.bottom = `-${100 * page}vh`;
-        if (page % 3 === 2) firstRef.current.style.top = `${100 * page + 100}vh`;
-        else if (page % 3 === 0) secondRef.current.style.top = `${100 * page + 100}vh`;
-        else if (page % 3 === 1) thirdRef.current.style.top = `${100 * page + 100}vh`;
-        // page++;
-        setPage(++page);
+        if (page % 3 === 1) {
+          thirdRef.current.style.top = `${100 * page + 100}vh`;
+          console.log('page3');
+          setPage3(page + 2);
+        } else if (page % 3 === 2) {
+          firstRef.current.style.top = `${100 * page + 100}vh`;
+          console.log('page1');
+          setPage1(page + 2);
+        } else if (page % 3 === 0) {
+          secondRef.current.style.top = `${100 * page + 100}vh`;
+          console.log('page2');
+          setPage2(page + 2);
+        }
+        page++;
       // 向下滑动
-      } else if (currDis > up && currDis > prevDis) {
-        // page--;
-        setPage(--page);
-        console.log('向下滑动');
-
+      } else if (currDis > down && currDis > prevDis) {
+        console.log('向下滑动', page);
         bottomRef.current.style.bottom = `-${100 * page}vh`;
-        if (page % 3 === 2) firstRef.current.style.top = `${100 * page - 100}vh`;
-        else if (page % 3 === 0) secondRef.current.style.top = `${100 * page - 100}vh`;
-        else if (page % 3 === 1) thirdRef.current.style.top = `${100 * page - 100}vh`;
+        if (page % 3 === 1) {
+          thirdRef.current.style.top = `${100 * page - 100}vh`;
+          console.log('page3');
+          setPage3(page );
+        } else if (page % 3 === 2) {
+          firstRef.current.style.top = `${100 * page - 100}vh`;
+          console.log('page1');
+          setPage1(page);
+        } else if (page % 3 === 0) {
+          secondRef.current.style.top = `${100 * page - 100}vh`;
+          console.log('page2');
+          setPage2(page );
+        }
+        page--;
       }
     });
   }
   useEffect(() => {
     handleScroll();
-  }, []);
+  }, [page]);
   return <ScrollView className="list-wrapper" ref={scrollRef}>
     <View className="page-wrap">
       <View className="top" ref={topRef}>到顶了～</View>
-      <View className="first page" ref={firstRef} >{page}</View>
-      <View className="second page" ref={secondRef} >{page + 1}</View>
-      <View className="third page" ref={thirdRef} >{page + 2}</View>
+      <View className="first page" ref={firstRef} >{page1}</View>
+      <View className="second page" ref={secondRef} >{page2}</View>
+      <View className="third page" ref={thirdRef} >{page3}</View>
       <View className="bottom" ref={bottomRef}>到底了～</View>
     </View>
     {/* {list.map(item => <View style={{width: '100vw', height: '20vw', border: '1vw #56bd6a solid', textAlign: 'center'}} key={item}>{item}</View>)} */}
