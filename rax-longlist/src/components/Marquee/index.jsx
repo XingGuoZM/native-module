79ad1ec420927b9bfecb0e095cf1892194c215c1
XingGuoZM <18969176324@163.com>
2021-09-15 19:40:03 +0800

    marquee

--- rax-longlist/src/components/Marquee/index.jsx
+++ rax-longlist/src/components/Marquee/index.jsx
@@ -1,37 +1,62 @@
-import { createElement, createRef, useEffect, useState} from 'rax';
+import { createElement, useEffect, useState, useRef } from 'rax';
 import View from 'rax-view';
 import './index.css';
 
-let num = 0;
-export default (props) => {
-  const {data} = props;
+export default ({ data }) => {
   const [percent, setPercent] = useState(0);
-  let [list, setList] = useState(data);
-  useEffect(() => {
-    setList(list.push(data[0]));
-
-    window.setInterval(() => {
-      let len = list.length;
-      let n = num % len / len * 100;
-      setPercent(n);
-      if (num % len === len - 1) {
-        window.setTimeout(() => {
-          setPercent(0);
-          num += 1;
-        }, 800);
-      }
-      num += 1;
-    }, 1000);
-  }, []);
-  function renderDefaultCell(item, index) {
-    return <View key={index} className="marquee-item">{item.name}</View>;
+  const timerRef = useRef();
+  /**
+   * 当data没有数据时或者data取不到则展示‘暂无数据’
+   * 当只有一条数据时，展示不滚动
+   *  当数据大于等于2，数组第一项复制一份至数组最末尾
+   * */
+  const len = data?.length;
+  if (len > 1) {
+    data = data.concat(data[0]);
   }
-  return <View className="marquee-wrap">
-    <View style={{
-      transform: `translate3d(0,-${percent}%,0)`,
-      transition: `${percent === 0 ? 'none' : 'transform .8s ease'}`
-    }}>
-      {data.map((item, index) => renderDefaultCell(item, index))}
+
+  useEffect(() => {
+    loop(0);
+    // 组件销毁时，清除定时器
+    return () => {
+      window.clearTimeout(timerRef.current);
+    }
+    // 监听数组变化，数组变化重新开始滚动
+  }, [len]);
+  const loop = (index) => {
+    if (timerRef.current) window.clearTimeout(timerRef.current);
+    timerRef.current = window.setTimeout(
+      () => {
+        const n = index % len / len * 100;
+        setPercent(n);
+        // 当滚动到最后一条时，不需要动画，并且要发生瞬间移动至最头部
+        if (len - 1 === index) {
+          setPercent(0);
+          index = 0;
+        }
+        index += 1;
+        loop(index);
+      },
+      len - 1 === index ? 800 : 3000
+    );
+  };
+  return (
+    <View className="marquee-wrap">
+      <View
+        style={{
+          // 跑马灯滚动动画（过渡动画），动画时长可调，.8s和loop函数里的800相对应
+          transform: `translate3d(0,-${percent}%,0)`,
+          transition: `${percent === 0 ? 'none' : 'transform .8s ease'}`
+        }}
+      >
+        {/* 数据存在且长度大于0才展示跑马灯*/}
+        {len > 0
+          ? data.map((item, index) => (
+            <View key={index} className="marquee-item">
+              {item}
+            </View>
+          )) : '暂无数据'}
+      </View>
     </View>
-  </View>;
+  );
 };
